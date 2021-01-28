21b215c54058c2966df55154cede48efd85a80a2
alexander-pecha <alexander.perepechko@gmail.com>
2021-01-28 23:02:17 +0300

    Update z7.java

--- src/z7.java
+++ src/z7.java
@@ -6,33 +6,40 @@ public class z7 {
         Random random = new Random();
         int[] mass = new int[10];
 
-        int pozitiv = 0;
-        int negativ = 0;
+        int Countpozitiv = 0;
+        int Countnegativ = 0;
         for( int i = 0; i < mass.length; i++){
             mass[i] = random.nextInt(10);
             if(mass[i] % 2 == 0 && mass[i] != 0){
-                pozitiv++;
+                Countpozitiv++;
             }
             if(mass[i] % 2 == 1 && mass[i] != 0){
+                Countnegativ++;
+            }
+        }
+        int[] massPozitiv = new int[Countpozitiv];
+        int pozitiv = 0;
+
+        for(int i = 0; i < mass.length; i++){
+            if(mass[i] % 2 == 0 && mass[i] != 0){
+                massPozitiv[pozitiv] = mass[i];
+                pozitiv++;
+            }
+        }
+        int[] massNegativ = new int[Countnegativ];
+        int negativ = 0;
+
+        for(int i = 0; i < mass.length; i++){
+            if(mass[i] % 2 == 1 && mass[i] != 0){
+                massNegativ[negativ] = mass[i];
                 negativ++;
             }
         }
-        int[] mass1 = new int[pozitiv];
-
-        for(int i = 0; i < mass.length; i++){
-                mass1[pozitiv] = mass[i];
-                pozitiv++;
-
-        }
-        int[] mass2 = new int[negativ];
-
-        for(int i = 0; i < mass.length; i++){
-            mass2[negativ] = mass[i];
-            negativ++;
-        }
-
         System.out.println(Arrays.toString(mass));
-        System.out.println(Arrays.toString(mass1));
-        System.out.println(Arrays.toString(mass2));
+        System.out.println(Countnegativ);
+        System.out.println(Countpozitiv);
+        System.out.println(Arrays.toString(massPozitiv));
+        System.out.println(Arrays.toString(massNegativ));
+
     }
 }
